cb2d988b39e4421b37b426e47b39ccc7a73c26fe
Evgheniy Kovaliov <111194749+kovaliovev@users.noreply.github.com>
2023-04-11 13:20:59 +0300

    add `isValide` method testing

--- test/IPv4.js
+++ test/IPv4.js
@@ -20,3 +20,20 @@ const runTests = (fn, tests) => {
   console.log(`Successfully passed: ${tests.length - failed}/${tests.length}`);
   console.log('-----------------------------');
 };
+
+const ip = new IPv4();
+
+const validationTests = [
+  ['123.123.123.123', true, 'Validation test #1'],
+  ['0.0.0.0', true, 'Validation test #2'],
+  ['255.255.255.255', true, 'Validation test #3'],
+  ['1.2.3.004', false, 'Validation test #4'],
+  ['253.254.255.256', false, 'Validation test #5'],
+  ['123.123.123', false, 'Validation test #6'],
+  ['error-string', false, 'Validation test #7'],
+  ['10.0.0.1', true, 'Validation test #8'],
+  ['10.0.0.one', false, 'Validation test #9'],
+  ['127.0.0.0.10', false, 'Validation test #10'],
+];
+
+runTests(ip.isValide, validationTests);
