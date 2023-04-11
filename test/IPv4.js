a60d8ad5ec742aebbe115ea8210d56b0fbdc5775
Evgheniy Kovaliov <111194749+kovaliovev@users.noreply.github.com>
2023-04-11 13:18:44 +0300

    implemented test-runner

--- test/IPv4.js
+++ test/IPv4.js
@@ -1,4 +1,22 @@
 'use strict';
 
-const assert = require('node:assert');
+const assert = require('node:assert/strict');
 const { IPv4 } = require('../lib/IPv4.js');
+
+const runTests = (fn, tests) => {
+  console.log('Testing started!');
+  let failed = 0;
+  for (const [input, excepted, name] of tests) {
+    console.log(`${name} testing...`);
+    const output = fn(input);
+    try {
+      assert.deepStrictEqual(output, excepted, name);
+    } catch (err) {
+      if (err) console.error(err);
+      failed++;
+    }
+  }
+  console.log('Testing ended!');
+  console.log(`Successfully passed: ${tests.length - failed}/${tests.length}`);
+  console.log('-----------------------------');
+};
