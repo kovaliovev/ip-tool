fc5c8501401c5e01ca3db9ad2b1b45ef357e83fe
Evgheniy Kovaliov <kovaliov.evgeniy11@gmail.com>
2023-06-04 18:20:55 +0300

    add `isValide` method testing

--- test/IPv6.js
+++ test/IPv6.js
@@ -5,6 +5,39 @@ const { runTests } = require('./test-runner.js');
 
 const ipv6 = new IPv6();
 
-const allTests = [];
+const ipValidationTest = [
+  {
+    testName: 'Ip address validation',
+    context: ipv6,
+    fn: ipv6.isValide,
+  },
+  ['::', true],
+  ['::1', true],
+  ['1::1:1', true],
+  ['0000:0000:0000:0000:0000:0000:0000::', true],
+  ['0000:0000:0000:0000:0000:0000:0000:0000', true],
+  ['0:00:000:0000:000:00:0::', true],
+  ['FFFF:FFFF:FFFF:FFFF:FFFF:FFFF:FFFF:FFFF', true],
+  ['7759:f8bf:2d23:7be9:25fb:adab:b6b5:b7a9', true],
+  ['7759::b7a9', true],
+  ['10.0.0.1', false],
+  ['0.0.0.0.0.0.0.0', false],
+  ['error:string', false],
+  ['', false],
+  [':', false],
+  [':::', false],
+  ['a:a', false],
+  ['::a:', false],
+  [':10AF:', false],
+  ['a:a:a:a:a:a:a:a:a', false],
+  ['0000::0000:0000:0000::', false],
+  ['FFFF:FFFF:FFFF:FFFF:FFFF:FFFF:FFFF:FZFF', false],
+  ['7759:f8bf:2d23:7be9:25fb:adab:b6b5:b7a9:56aa', false],
+  [['7709', 'f8ff', '2d3', '7e9', '20b', 'adb', 'b605', 'b7a0', '56af'], false],
+  [{}, false],
+  [0xffffffffffff, false],
+];
+
+const allTests = [ipValidationTest];
 
 runTests(allTests);
