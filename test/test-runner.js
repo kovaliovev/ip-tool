964ca5c1233fece7c2e417961b7b0d4108e7f2a8
Evgheniy Kovaliov <kovaliov.evgeniy11@gmail.com>
2023-12-27 23:25:53 +0200

    separate logging into `Logger` class

--- test/test-runner.js
+++ test/test-runner.js
@@ -4,11 +4,7 @@ const assert = require('node:assert/strict');
 
 class TestRunner {
   constructor() {
-     this.colors = {
-      default: '\x1b[0m',
-      red: '\x1b[31m',
-      green: '\x1b[32m',
-    };
+    this.logger = new Logger();
      this.errorPrefix = 'ERROR:';
   }
 
@@ -18,18 +14,14 @@ class TestRunner {
       const testInfo = testSuit[0];
       const { testName, context, fn } = testInfo;
       const caseCount = testSuit.length - 1;
-      console.log(`${testName} testing started!`);
+      this.logger.start(testName);
 
       for (let i = 1; i <= caseCount; i++) {
         const testCaseName = `${testName} test #${i}`;
         const success = await this.runCase(fn, context, testSuit[i], testCaseName);
         if (!success) failed++;
       }
-      const passed = caseCount - failed;
-      const color = passed === caseCount ? this.colors['green'] : this.colors['red'];
-      const resetColor = this.colors['default'];
-      console.log(`${color} Successfully passed: ${passed}/${caseCount}${resetColor}`);
-      console.log('-----------------------------');
+      this.logger.result(caseCount, failed);
     }
   }
 
@@ -44,12 +36,40 @@ class TestRunner {
     } catch (err) {
       const isExcepted = typeof excepted === 'string' && excepted.startsWith(this.errorPrefix);
       if (!isExcepted || err.message !== excepted.slice(this.errorPrefix.length)) {
-        console.log(`${testCaseName} failed!`);
-        console.error(err);
+        this.logger.fail(testCaseName, err);
         return false;
       }
     }
     return true;
+  }
+}
+
+class Logger {
+  constructor() {
+    this.colors = {
+      default: '\x1b[0m',
+      red: '\x1b[31m',
+      green: '\x1b[32m',
+    };
+  }
+
+  start(testName){
+    console.log(`${testName} testing started!`);
+  }
+
+  fail(testName, err) {
+    console.log(`${testName} failed!`);
+    console.error(err);
+  }
+
+  result(testCount, failed){
+    const passed = testCount - failed;
+    const color = passed === testCount ?
+      this.colors['green'] :
+      this.colors['red'];
+    const resetColor = this.colors['default'];
+    console.log(`${color} Successfully passed: ${passed}/${testCount}${resetColor}`);
+    console.log('-----------------------------');
   }
 }
 
